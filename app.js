84fab794c95f71e396d26dbbeb8fb4a54207ba03
Nazarioo-hub <apedronazario@gmail.com>
2025-11-20 10:38:04 +0000

    Mais um update

--- app.js
+++ app.js
@@ -545,6 +545,43 @@ async function uploadFileToDrive(accessToken) {
 }
 
 
+// NOVOS TESTES
+
+async function listDriveJsonFiles() {
+  // Garante que o token está disponível
+  const accessToken = tokenClient.access_token;
+  if (!accessToken) {
+    alert('Token de acesso não disponível. Autentique-se!');
+    return;
+  }
+
+  // Faz a chamada à API listando apenas arquivos JSON
+  const response = await fetch(
+    'https://www.googleapis.com/drive/v3/files?q=mimeType="application/json"&fields=files(id,name)',
+    {
+      headers: { 'Authorization': `Bearer ${accessToken}` }
+    }
+  );
+
+  const data = await response.json();
+
+  // Renderiza os nomes e ids no select do modal
+  const select = document.getElementById('driveFileList');
+  select.innerHTML = '';
+  
+  if (data.files && data.files.length > 0) {
+    data.files.forEach(file => {
+      const option = document.createElement('option');
+      option.value = file.id;
+      option.textContent = file.name;
+      select.appendChild(option);
+    });
+  } else {
+    const option = document.createElement('option');
+    option.textContent = 'Nenhum ficheiro JSON encontrado.';
+    select.appendChild(option);
+  }
+}
 
 
 
